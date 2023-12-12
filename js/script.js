9cc345ba25485321246986a397b811bd84437caa
Michelazani <michi.zani220@gmail.com>
2023-12-12 16:52:03 +0100

    generazione 10 indirizzi email

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            mail:'',
+            newMail : []
+        }
+    },
+    methods: {
+        randomMails(){
+            // CREO CICLO PER VISUALIZZARE MAIL
+            for (let i= 0; i <10 ; i++){
+                // METTO HTTP DELLA PAGINA 
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(
+                    // ARROW FUNCTION FONDAMENTALE 
+                    (response) => {
+                    console.log(response);
+                    // inserisco nell'array vuoto le mail
+                    this.newMail.push(response.data.response); 
+                });
+            }
+        }
+    },
+
+    // per richiamare il method
+    created (){
+        this.randomMails()
+    }
+}).mount('#app'); 
+
